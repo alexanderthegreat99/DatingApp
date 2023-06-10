c382f882dd9e3bd4c41c295fc36811bce0ba1681
alexanderthegreat99 <darren.alex.perez>
2023-06-09 22:41:50 -0400

    End of section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,16 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { NgToastService } from 'ng-angular-popup';
-
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,23 +19,70 @@ import { NgToastService } from 'ng-angular-popup';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {}
+  registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
 
-  constructor(private accountService: AccountService, private toast: NgToastService) { }
+  constructor(private accountService: AccountService, private toast: NgToastService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
+  }
+  // initializeForm() {
+  //   this.registerForm = this.fb.group({
+  //     gender: ['male'],
+  //     username: ['', Validators.required],
+  //     knownAs: ['', Validators.required],
+  //     dateOfBirth: ['', Validators.required],
+  //     city: ['', Validators.required],
+  //     country: ['', Validators.required],
+  //     password: ['', [Validators.required, 
+  //       Validators.minLength(4), Validators.maxLength(8)]],
+  //     confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+  //   });
+  //   this.registerForm.controls['password'].valueChanges.subscribe({
+  //     next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+  //   })
+  // }
+   initializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, 
+      Validators.minLength(4), Validators.maxLength(8)]], 
+      
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth: dob};
+    //console.log(values);
+    this.accountService.register(values).subscribe({
       next: () => {
-        this.cancel();
+        //this.cancel();
+        this.router.navigateByUrl('/members')
         this.toast.success({detail:"SUCCESS",summary:'Login Sucess!', duration: 5000});
       },
       error: error => {
         console.log("error: ", error);
+        this.validationErrors = error;
 
-        this.toast.error({detail:"ERROR",summary:'Register failed! Try again.', duration: 5000});
+       // this.toast.error({detail:"ERROR",summary:'Register failed! Try again.', duration: 5000});
 
       }
     })
@@ -34,6 +90,13 @@ export class RegisterComponent implements OnInit {
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(dob: string | undefined) {
+    if (!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes()-theDob.getTimezoneOffset()))
+      .toISOString().slice(0,10);
   }
 
 }
