25d9e72332559c2714a2b92f2588c67d55842c54
alexanderthegreat99 <darren.alex.perez>
2023-06-15 13:10:33 -0400

    End of section 14

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
 import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-card',
@@ -10,9 +12,15 @@ import { Member } from 'src/app/_models/member';
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private toast: NgToastService) { }
 
   ngOnInit(): void {
   }
+  addLike(member: Member){
+    this.memberService.addLike(member.userName).subscribe({
+      next: () => this.toast.success({detail:"SUCCESS",summary:'You have liked ' + member.knownAs, duration: 5000})
+    })
+  }
+
 
 }
